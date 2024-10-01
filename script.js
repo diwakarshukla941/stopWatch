e01af0bf8968d523f8b8caf0bc0addf739ffa33b
diwakarshukla941 <shukladiwakar941@gmail.com>
2024-10-01 22:44:27 +0530

    added footed

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const displayTime = document.getElementById('display');
-let [seconds,minutes,hours] = [0,0,0];
+let [seconds,minutes,hours,milisecond] = [0,0,0,0];
 let timer = null;
 
 const stopWatch =() =>{
-    seconds++;
-    if(seconds == 60){
-        seconds = 0;
-        minutes++;
-        if(minutes == 60){
-            minutes = 0;
-            hours++;
-        }
+    milisecond++;
+    if(milisecond == 60){   
+        milisecond = 0; 
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
     }
-
-    let h = hours < 10 ? "0"+hours:hours;
-    let m = minutes < 10 ? "0"+minutes:minutes;
-    let s = seconds < 10 ? "0"+seconds:seconds;
+    }
+    let ms = milisecond < 10 ? "0" + milisecond : milisecond;
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
     
-    displayTime.innerHTML = h +":"+ m +":"+ s;
+    displayTime.innerHTML = h +":"+ m +":"+ s + ":" + ms;
 
 }
 
 function startWatch(){
-    if(timer != null){
-        clearInterval(timer)
-    }
-    timer = setInterval(stopWatch,1000);
+        timer = setInterval(stopWatch,1000);
 }
 
 function watchStop(){
@@ -35,5 +36,5 @@ function watchStop(){
 function watchReset(){
     clearInterval(timer);
     [seconds,minutes,seconds] = [0,0,0]
-    displayTime.innerHTML = "00:00:00";
+    displayTime.innerHTML = "00:00:00:00";
 }
